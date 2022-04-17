6ebc02ac1b0ba05558498e7deca79d5b150954ad
asbelljc <asbelljc@gmail.com>
2022-04-16 20:14:19 -0400

    Put in rudimentary username/password inputs and functions to test connection with auth routes on backend.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
+const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+
 const SessionContext = createContext(null);
 
 function App() {
   const [session, setSession] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     async function syncSession() {
       // calling /auth/session endpoint returns the server's session status (user, admin, or null)
-      const { session } = await fetch(
-        `${process.env.REACT_APP_BACKEND_BASE_URL}/auth/session`
-      ).then((res) => res.json());
+      const { session } = await fetch(`${backendUrl}/auth/session`).then(
+        (res) => res.json()
+      );
 
       setSession(session);
     }
@@ -18,7 +22,81 @@ function App() {
     syncSession().catch((err) => console.log(err));
   }, []);
 
-  return <div className="App"></div>;
+  // TESTING
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
+  async function login(username, password) {
+    try {
+      const response = await fetch(`${backendUrl}/auth/login`, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.status === 401) {
+        return console.log('Credentials invalid.');
+      } else if (!response.ok) {
+        return console.log('Something went wrong.');
+      } else {
+        await response.json().then((res) => {
+          setSession(res.session);
+          console.log('Logged in.');
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function logout() {
+    try {
+      const response = await fetch(`${backendUrl}/auth/logout`, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      });
+
+      if (!response.ok) {
+        return console.log('Something went wrong.');
+      } else {
+        console.log('Logout worked.');
+        await response.json().then((res) => setSession(null));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <SessionContext.Provider value={{ session, login, logout }}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={() => login(username, password)}>Log In</button>
+      <button onClick={logout}>Log Out</button>
+      {session === 'admin' ? (
+        <h1>ADMIN</h1>
+      ) : session === 'user' ? (
+        <h1>USER</h1>
+      ) : (
+        <h1>NULL</h1>
+      )}
+    </SessionContext.Provider>
+  );
 }
 
 export default App;
