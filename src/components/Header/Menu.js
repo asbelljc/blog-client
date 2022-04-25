80f3d29f22877a73fe822de6e9b88b0be83fc0f3
asbelljc <asbelljc@gmail.com>
2022-04-25 17:50:39 -0400

    Continued styling login/signup menu.

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -2,28 +2,86 @@ import { useContext } from 'react';
 import { SessionContext, ScreenContext } from '../../App';
 import styled from 'styled-components';
 
-const UserButton = styled.button`
-  border: none;
-  background: ${({ theme }) => theme.colors.blue};
-  border-radius: 5px;
-  padding: 10px 20px;
-  color: white;
-  font-weight: bold;
-  transition: background 150ms;
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 8px;
+  width: 90%;
+  height: 60px;
 
-  &:hover {
-    background: ${({ theme }) => theme.colors.blueGlow};
+  /* horizonatal divider at top */
+  :before {
+    position: absolute;
+    top: 0;
+    content: '';
+    height: 1px;
+    width: 100%;
+    background: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 0) 0,
+      rgba(0, 0, 0, 0.2) 50%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
+
+  @media screen and (min-width: 800px) {
+    & {
+      width: min(80%, 1000px);
+    }
   }
 `;
 
+const UserButton = styled.button`
+  border: 1px solid ${({ theme }) => theme.colors.blue};
+  background: ${(props) => (props.solid && props.theme.colors.blue) || 'none'};
+  border-radius: 5px;
+  padding: 10px 20px;
+  color: ${(props) =>
+    (props.solid && props.theme.colors.white) || props.theme.colors.blue};
+  font-weight: bold;
+  transition: background 150ms;
+  cursor: pointer;
+
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.colors.blueGlow};
+    background: ${(props) =>
+      (props.solid && props.theme.colors.blueGlow) || 'none'};
+    color: ${(props) =>
+      (props.solid && props.theme.colors.white) || props.theme.colors.blueGlow};
+  }
+`;
+
+const Greeting = styled.span`
+  display: block;
+`;
+
+const LoginField = styled.input``;
+
 function Menu() {
-  const { session } = useContext(SessionContext);
+  const { session, login, logout } = useContext(SessionContext);
   const { isDesktop } = useContext(ScreenContext);
 
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <Wrapper>
+      {!session ? (
+        <>
+          <LoginField type="text" placeholder="Username" />
+          <LoginField type="password" placeholder="Password" />
+          <UserButton>Log In</UserButton>
+          <UserButton solid>Sign Up</UserButton>
+        </>
+      ) : (
+        <>
+          <Greeting>Hello, {session.username}!</Greeting>
+          {session.status === 'admin' ? (
+            <UserButton solid>New Post</UserButton>
+          ) : null}
+          <UserButton>Log Out</UserButton>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,38 +1,42 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import styled, { useTheme } from 'styled-components';
+import styled from 'styled-components';
 import Menu from './Menu';
 
-const OuterHeader = styled.header`
+const Wrapper = styled.header`
   display: flex;
+  flex-direction: column;
   position: fixed;
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 80px;
   background: white;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 `;
 
-const InnerHeader = styled.div`
+const Bar = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 90%;
-  height: 100%;
+  height: 80px;
 
   @media screen and (min-width: 800px) {
     & {
       width: min(80%, 1000px);
     }
   }
+`;
 
-  a {
-    font-size: 32px;
-    font-weight: bold;
-    text-decoration: none;
-    user-select: none;
+const Brand = styled.a`
+  font-size: 32px;
+  font-weight: bold;
+  text-decoration: none;
+  user-select: none;
+
+  span {
+    color: ${({ theme }) => theme.colors.blue};
   }
 `;
 
@@ -59,25 +63,22 @@ const MenuButton = styled.button`
 `;
 
 function Header() {
-  const theme = useTheme();
-
   const [isMenuOpen, setMenuOpen] = useState(false);
+
   const toggleMenu = () => setMenuOpen(!isMenuOpen);
 
   return (
-    <>
-      <OuterHeader>
-        <InnerHeader>
-          <a href="/">
-            code<span style={{ color: `${theme.colors.blue}` }}>Blog</span>
-          </a>
-          <MenuButton onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} size="xl" />
-          </MenuButton>
-        </InnerHeader>
-      </OuterHeader>
+    <Wrapper>
+      <Bar>
+        <Brand href="/">
+          code<span>Blog</span>
+        </Brand>
+        <MenuButton onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} size="xl" />
+        </MenuButton>
+      </Bar>
       {isMenuOpen ? <Menu /> : null}
-    </>
+    </Wrapper>
   );
 }
 
